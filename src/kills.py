0d4536572eda1c49cf210ff5de0525a82b1e0492
ChillerDragon <ChillerDragon@gmail.com>
2018-03-25 23:26:59 +0200

    Get killer and victim names

--- src/kills.py
+++ src/kills.py
@@ -2,5 +2,31 @@
 from chiller_essential import *
 
 def HandleKills(data):
-    if (data.find("kill killer=")):
-        say("KILL!")
+    #sample kill messages:
+    #[game]: kill killer='0:ChillerDragon' victim='1:ChillerDragon.*' weapon=3 special=0
+    #[game]: kill killer='0:A' victim='1:ChillerDragon.*' weapon=3 special=0
+    #0123456789111111111122222222223333333333
+    #          012345678901234567890123456789
+
+    # Could do: use regex by chuck norris
+    # https://github.com/chuck-norris-network/teeworlds-econ-js/blob/79e6d668ee64aaaf3a714297a3c726a1867c974f/src/handlers/handle-kill-message.coffee
+    """
+    regex = re.compile("^\[game\]: kill killer='(?P<killer_id>[0-9]+):(.+?)' victim='([0-9]+)+:(.+?)' weapon=([0-9-]+) special=[0-9]+$")
+    m = regex.match("[game]: kill killer='0:ChillerDragon' victim='1:ChillerDragon.*' weapon=3 special=0")
+    if m:
+        print(m.group("killer_id"))
+    """
+
+
+
+    killer_start = data.find(":", 10) + 1
+    killer_end = data.find("' victim='", killer_start + 1)
+    killer_name = data[killer_start:killer_end]
+
+
+    victim_start = data.find(":", killer_end) + 1
+    victim_end = data.find("' weapon=", victim_start + 1)
+    victim_name = data[victim_start:victim_end]
+
+
+    say("[KILL] killer=" + killer_name + " victim=" + victim_name)
--- src/main.py
+++ src/main.py
@@ -2,12 +2,13 @@
 import sys
 import time
 from chat import *
-
+from kills import *
 
 def HandleData(data):
     HandleChatMessage(data)
     if (data.startswith("[register]")):
-        say("register found: " + data)        
+        #say("register found: " + data) #working but was only useless chat spam for testing        
+        pass
     elif (data.startswith("[Console]")):
         if (data.find("No such command")):
             return
@@ -17,12 +18,12 @@ def HandleData(data):
         say("chat message: " + data)
     elif (data.startswith("[game]")):
         if (data.find("kill killer") != -1):
-            say("KILL: " + data)
+            HandleKills(data)    
 
 try:
     while True:
 
-        time.sleep(1)
+        #time.sleep(1)
 
         line = sys.stdin.readline()
         if not line:
