d4545c2893e5cf45b14a5e9037e1cc971053da77
ChillerDragon <ChillerDragon@gmail.com>
2019-09-19 18:09:01 +0200

    Colorized tests and fail counter

--- test/test.sh
+++ test/test.sh
@@ -8,11 +8,33 @@ function print_log_lines() {
     done < $1
 }
 
+failed=0
+passed=0
+total=0
+
 for log in $(ls logs/*)
 do
     echo   "+---------------------------------------+"
     printf "| log: %-32s |\n" $log
     echo   "+---------------------------------------+"
     print_log_lines $log | ../src/main.py --settings=../tem.settings
+    if [ $? -eq 0 ]
+    then
+        printf "[\033[0;32mSUCCESS\033[0m]\n"
+        passed=$((passed+1))
+    else
+        printf "[\033[0;31mFAILED\033[0m]\n"
+        failed=$((failed+1))
+    fi
+    total=$((total+1))
 done
 
+echo " --------------------------------------- "
+echo ""
+printf "failed: \033[0;31m$failed/$total\033[0m\n"
+printf "passed: \033[0;32m$passed/$total\033[0m\n"
+
+if [ $failed -gt 0 ]
+then
+    exit 1
+fi
