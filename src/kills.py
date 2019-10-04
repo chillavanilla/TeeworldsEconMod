c753e7230df236d9b4f7618a533006a026922657
ChillerDragon <ChillerDragon@gmail.com>
2019-10-04 20:25:42 +0200

    Add more stable regex parsing for 0.7 kills

--- src/kills.py
+++ src/kills.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import re
 from chiller_essential import *
 import cbase
 import g_settings
@@ -14,20 +15,9 @@ def HandleKills(data):
     weapon = ""
 
     if g_settings.get("tw_version") == 6 or g_settings.get("tw_version") == None: # default 6
-        #sample kill messages:
-        #[game]: kill killer='0:ChillerDragon' victim='1:ChillerDragon.*' weapon=3 special=0
-        #[game]: kill killer='0:A' victim='1:ChillerDragon.*' weapon=3 special=0
-        #0123456789111111111122222222223333333333
-        #          012345678901234567890123456789
-
-        # Could do: use regex by chuck norris
-        # https://github.com/chuck-norris-network/teeworlds-econ-js/blob/79e6d668ee64aaaf3a714297a3c726a1867c974f/src/handlers/handle-kill-message.coffee
-        """
-        regex = re.compile("^\[game\]: kill killer='(?P<killer_id>[0-9]+):(.+?)' victim='([0-9]+)+:(.+?)' weapon=([0-9-]+) special=[0-9]+$")
-        m = regex.match("[game]: kill killer='0:ChillerDragon' victim='1:ChillerDragon.*' weapon=3 special=0")
-        if m:
-            echo(m.group("killer_id"))
-        """
+        # sample kill messages:
+        # [game]: kill killer='0:ChillerDragon' victim='1:ChillerDragon.*' weapon=3 special=0
+        # [game]: kill killer='0:A' victim='1:ChillerDragon.*' weapon=3 special=0
 
         killer_start = data.find(":", 10) + 1
         killer_end = data.find("' victim='", killer_start + 1)
@@ -46,43 +36,53 @@ def HandleKills(data):
         # teeworlds 0.7
         #                     id:team                   id:team
         # [game]: kill killer='0:0:nameless tee' victim='0:0:nameless tee' weapon=-1 special=0
-        killer_start = cbase.cfind(data, ":", 3) + 1
-        killer_end = data.find("' victim='", killer_start + 1)
-        killer_name = data[killer_start:killer_end]
+        # [game]: kill killer='-2:1:' victim='0:0:ChillerDragon' weapon=3 special=0
 
-        victim_start = data.find(":", killer_end) + 1
-        victim_start = data.find(":", victim_start) + 1
-        victim_end = data.find("' weapon=", victim_start + 1)
-        victim_name = data[victim_start:victim_end]
+        data = data[:-1]
+        #                                                                                       use .* for killer but .+? for victim because killer can be empty if it left the server before the projectile hit
+        m = re.match(r"^\[game\]: kill killer='(?P<k_id>-?\d{1,2}):(?P<k_team>-?\d{1,2}):(?P<k_name>.*)' victim='(?P<v_id>-?\d{1,2}):(?P<v_team>-?\d{1,2}):(?P<v_name>.+?)' weapon=(?P<weapon>-?\d) special=(\d)$", data)
 
-        killer_start = data.find("'") + 1
-        killer_end = cbase.cfind(data, ":", 2)
-        killer_id = data[killer_start:killer_end]
-        killer = player.GetPlayerByID(killer_id)
+        if m:
+            if g_settings.get("debug"):
+                say("KILLER ID=<%s> TEAM=<%s> NAME=<%s> VICTIM ID=<%s> TEAM=<%s> NAME=<%s> weapon=<%s>" % (
+                        m.group("k_id"), m.group("k_team"), m.group("k_name"),
+                        m.group("v_id"), m.group("v_team"), m.group("v_name"),
+                        m.group("weapon")
+                    )
+                )
+            if m.group("k_name") == "" or int(m.group("k_id")) < 0:
+                killer_name = "a left player"
+                killer_id = m.group("k_id")
+                killer = None
+            else:
+                killer_name = m.group("k_name")
+                killer_id = m.group("k_id")
+                killer = player.GetPlayerByID(killer_id)
 
-        victim_start = data.find("' victim='") + len("' victim='")
-        victim_end = data.find(":", victim_start)
-        victim_id = data[victim_start:victim_end]
-        victim = player.GetPlayerByID(victim_id)
+            victim_name = m.group("v_name")
+            victim_id = m.group("v_id")
+            victim = player.GetPlayerByID(victim_id)
 
-        weapon_start = data.rfind("weapon=") + 7
-        weapon_end = data.rfind(" special=")
-        weapon = data[weapon_start:weapon_end]
-
-        # TODO: use regex or wildcard.. anything
-        if (data.startswith("[game]: kill killer='-1:0:' victim='") or
-            data.startswith("[game]: kill killer='-2:0:' victim='")):
-            killer_id = -1
-            killer = None
-            killer_name = "a left player"
+            weapon = m.group("weapon")
+        else:
+            say("[ERROR] failed parsing kill msg: " + str(data))
+            sys.exit(1)
 
     if not killer == victim and killer: # don't count suicide as kill or when killer left already
         if not player.UpdatePlayerKills(killer, 1, int(weapon)):
-            say("error adding kill for '" + str(killer_id) + ":" + killer_name + "'")
+            say("[ERROR] failed adding kill:")
+            say("   ID=" + str(killer_id))
+            say("   NAME=" + str(killer_name))
+            say("   OBJ=" + str(killer))
+            say("   DATA=" + data)
             sys.exit(1)
-    if not str(weapon) == "-3": #don't count disconnect or teamswitch as death
+    if not str(weapon) == "-3": # don't count disconnect or teamswitch as death
         if not player.UpdatePlayerDeaths(victim, killer_name, 1):
-            say("error adding death for '" + str(victim_id) + ":" + victim_name + "'")
+            say("[ERROR] failed adding death:")
+            say("   ID=" + str(victim_id))
+            say("   NAME=" + str(victim_name))
+            say("   OBJ=" + str(victim))
+            say("   DATA=" + data)
             sys.exit(1)
 
     # say("[KILL] killer=" + killer_name + " victim=" + victim_name + " weapon=" + str(weapon))
