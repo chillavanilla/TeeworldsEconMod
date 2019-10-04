e53b15222026c3e6a4f28d844065eb4a0cbf570b
ChillerDragon <ChillerDragon@gmail.com>
2019-10-04 22:15:54 +0200

    Add option for verbose tests

--- test/test.sh
+++ test/test.sh
@@ -2,17 +2,57 @@
 
 delay=0
 
+verbose=0
+logs_path=logs
+settings_path=settings
+
+RESET="\033[0m"
+BOLD="\033[1m"
+
+function set_paths() {
+    if [ $# -gt 0 ]
+    then
+        if [ ! -d "$1" ]
+        then
+            echo "Error logs path not found '$1'"
+            exit 1
+        fi
+        logs_path="$1"
+        echo "using custom logs path: $1"
+    fi
+    if [ $# -gt 1 ]
+    then
+        if [ ! -d "$2" ]
+        then
+            echo "Error settings path not found '$2'"
+            exit 1
+        fi
+        settings_path="$2"
+        echo "using custom settings path: $2"
+    fi
+}
+
 if [ "$1" == "--help" ] || [ "$1" == "-h" ]
 then
-    echo "usage: $0 [ log directory ] [ settings directory ]"
-    echo "  options:"
-    echo "    log dir      - path to directory containing tw .log files"
-    echo "                   default: logs"
-    echo "    settings dir - path to directory containing tem .test files"
-    echo "                   default: settings"
-    echo "  description:"
-    echo "    pipes all logs-settings combinations into tem python script."
+    echo    "usage: $0 [OPTION] [LOG_DIR] [SETTINGS_DIR]"
+    echo    "  arguments:"
+    echo -e "    OPTION       - ${BOLD}-h${RESET} show this help page."
+    echo -e "                   ${BOLD}-v${RESET} verbose output."
+    echo -e "                   ${BOLD}--help${RESET} equivalent to ${BOLD}-h${RESET}"
+    echo -e "                   ${BOLD}--verbose${RESET} equivalent to ${BOLD}-v${RESET}"
+    echo    "    LOG DIR      - path to directory containing tw .log files"
+    echo    "                   default: logs"
+    echo    "    SETTINGS DIR - path to directory containing tem .test files"
+    echo    "                   default: settings"
+    echo    "  description:"
+    echo    "    pipes all logs-settings combinations into tem python script."
     exit
+elif [ "$1" == "--verbose" ] || [ "$1" == "-v" ]
+then
+    verbose=1
+    set_paths $2 $3
+else
+    set_paths $1 $2
 fi
 
 function print_log_lines() {
@@ -42,41 +82,25 @@ function test_log() {
     printf "| log: %-32s |\n" $log
     echo   "+---------------------------------------+"
     log_lines=$(print_log_lines $log | ../src/main.py --settings=$setting)
+    show_lines=$verbose
     if [ $? -eq 0 ]
     then
         printf "[\033[0;32mSUCCESS\033[0m]\n"
         passed=$((passed+1))
     else
+        printf "[\033[0;31mFAILED\033[0m]\n"
+        failed=$((failed+1))
+        show_lines=1
+    fi
+    if [ "$show_lines" == "1" ]
+    then
         echo " === setting: $setting === "
         cat $setting
         echo " ================ "
         echo "$log_lines"
-        printf "[\033[0;31mFAILED\033[0m]\n"
-        failed=$((failed+1))
     fi
     total=$((total+1))
 }
-
-logs_path=logs
-settings_path=settings
-if [ $# -gt 0 ]
-then
-    if [ ! -d "$1" ]
-    then
-        echo "Errro logs path not found '$1'"
-        exit 1
-    fi
-    logs_path="$1"
-fi
-if [ $# -gt 1 ]
-then
-    if [ ! -d "$2" ]
-    then
-        echo "Errro settings path not found '$2'"
-        exit 1
-    fi
-    settings_path="$2"
-fi
 
 start_ts=`date +%s.%N`
 
