c593bee25bd6eb10c5513b3981314f899488874b
ChillerDragon <ChillerDragon@gmail.com>
2019-10-19 14:59:50 +0200

    Add discord bot names

--- src/discord/bot.py
+++ src/discord/bot.py
@@ -11,24 +11,31 @@ import requests
 import time
 
 token=""
+name=""
 
 def HandleData(line):
     global token
+    global name
     line = str(line)
     chat_str = "][chat]: "
     if (line.find(chat_str) == -1):
         return
     line_start = line.find(chat_str) + len(chat_str)
     line = line[line_start:]
+    if name != "":
+        line = "[" + name + "]" + line
     print("line: " + line)
     requests.post("https://discordapp.com/api/webhooks/" + token, data={"content": line})
 
 def main():
     global token
-    if (len(sys.argv) != 2):
-        print("usage: ./bot.py <discordtoken>")
+    global name
+    if (len(sys.argv) < 2):
+        print("usage: ./bot.py <discord_token> [<bot_name>]")
         sys.exit(1)
     token=sys.argv[1]
+    if (len(sys.argv) > 2):
+        name=sys.argv[2]
     try:
         while True:
             line = sys.stdin.readline()
--- tools/discord_bot.sh
+++ tools/discord_bot.sh
@@ -6,6 +6,7 @@
 
 # init variables
 settings_file="../tem.settings"
+name="nameless"
 aSettStr=();aSettVal=()
 aSettStr+=("sh_logs_path");aSettVal+=("/path/to/log/directory")
 aSettStr+=("py_discord_token");aSettVal+=("")
@@ -16,9 +17,19 @@ aSettStr+=("sh_econ_port");aSettVal+=("8203")
 aSettStr+=("sh_tw_cfg_file");aSettVal+=("")
 aSettStr+=("sh_tw_version");aSettVal+=("6")
 
+if [ $1 == "--help" ]
+then
+    echo "usage: $0 [<bot_name>] [<settings_file>]"
+    exit 0
+fi
+
 if [ $# -gt 0 ]; then
-    log "settings file=$1"
-    settings_file=$1
+    log "name=$1"
+    name=$1
+fi
+if [ $# -gt 1 ]; then
+    log "settings file=$2"
+    settings_file=$2
 fi
 
 function log() {
@@ -88,8 +99,9 @@ logpath="${aSettVal[0]}"
 token="${aSettVal[1]}"
 echo "token=${token:0:10}..."
 echo "logpath=$logpath"
+echo "name=$name"
 logfile=`ls $logpath | tail -1`
 log="$logpath$logfile"
 cd ../src/discord/
-tail -fn1 $log | PYTHONIOENCODING="UTF-8" ./bot.py $token
+tail -fn1 $log | PYTHONIOENCODING="UTF-8" ./bot.py $token $name
 
