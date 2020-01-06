e5e4a523185be21a3df32eba50202c6d152cf14b
ChillerDragon <ChillerDragon@gmail.com>
2020-01-06 10:13:05 +0100

    Fix inversed find for option votes

--- src/votes.py
+++ src/votes.py
@@ -4,7 +4,7 @@ from chiller_essential import *
 # [2020-01-04 15:31:47][server]: '1:zilly dummy' voted kick '0:ChillerDragon' reason='No reason given' cmd='ban 10.52.176.91 5 Banned by vote' force=0
 def HandleCallVote(data):
     # TODO: use regex here to avoid false positives
-    if data.find("' voted option '"):
+    if data.find("' voted option '") != -1:
         if g_settings.get("debug"):
             say("[VOTES] skip force on option vote")
         return
